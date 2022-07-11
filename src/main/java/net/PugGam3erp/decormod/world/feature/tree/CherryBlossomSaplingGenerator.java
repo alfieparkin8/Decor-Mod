9945e96f30757e33f426d6673bc9591de0f65023
puggam3er <69014858+puggam3er@users.noreply.github.com>
2022-07-11 22:02:32 +0100

    1.19!

--- src/main/java/net/PugGam3erp/decormod/world/feature/tree/CherryBlossomSaplingGenerator.java
+++ src/main/java/net/PugGam3erp/decormod/world/feature/tree/CherryBlossomSaplingGenerator.java
@@ -2,11 +2,11 @@ package net.PugGam3erp.decormod.world.feature.tree;
 
 import net.PugGam3erp.decormod.world.feature.ModConfiguredFeatures;
 import net.minecraft.block.sapling.SaplingGenerator;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Random;
 
 public class CherryBlossomSaplingGenerator extends SaplingGenerator {
     @Nullable
--- src/main/java/net/PugGam3erp/decormod/world/gen/ModTreeGeneration.java
+++ src/main/java/net/PugGam3erp/decormod/world/gen/ModTreeGeneration.java
@@ -4,11 +4,12 @@ import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.PugGam3erp.decormod.world.feature.ModPlacedFeatures;
 import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.gen.GenerationStep;
 
 public class ModTreeGeneration {
     public static void generateTrees() {
-        BiomeModifications.addFeature(BiomeSelectors.categories(Biome.Category.FOREST),
+        BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.FOREST),
                 GenerationStep.Feature.VEGETAL_DECORATION, ModPlacedFeatures.CHERRY_BLOSSOM_PLACED.getKey().get());
 
     }
