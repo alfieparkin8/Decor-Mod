6e94c553d79dee3127cbbcc9a58ca947aebad15a
puggam3er <alfieparkin8@gmail.com>
2022-07-17 18:32:38 +0100

    added potted plants to plant pots

--- src/main/java/net/PugGam3erp/decormod/util/ModUtil.java
+++ src/main/java/net/PugGam3erp/decormod/util/ModUtil.java
@@ -1,11 +1,12 @@
 package net.PugGam3erp.decormod.util;
 
+import net.PugGam3erp.decormod.block.ModBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 
 public class ModUtil {
     public static final String[] COLORS = new String[]{"white", "orange", "magenta", "light_blue", "yellow", "lime", "pink", "gray", "light_gray", "cyan", "purple", "blue", "brown", "green", "red", "black"};
-    public static final String[] PLANT_STRINGS = new String[]{"oak_sapling", "spruce_sapling", "birch_sapling", "jungle_sapling", "acacia_sapling", "dark_oak_sapling", "fern", "dandelion", "poppy", "blue_orchid", "allium", "azure_bluet", "bamboo", "red_tulip", "orange_tulip", "white_tulip", "pink_tulip", "oxeye_daisy", "cornflower", "lily_of_the_valley", "wither_rose", "red_mushroom", "brown_mushroom", "dead_bush", "cactus"};
+    public static final String[] PLANT_STRINGS = new String[]{"oak_sapling", "spruce_sapling", "birch_sapling", "jungle_sapling", "acacia_sapling", "dark_oak_sapling", "fern", "dandelion", "poppy", "blue_orchid", "allium", "azure_bluet", "bamboo", "red_tulip", "orange_tulip", "white_tulip", "pink_tulip", "oxeye_daisy", "cornflower", "lily_of_the_valley", "wither_rose", "red_mushroom", "brown_mushroom", "dead_bush", "cactus","cherry_blossom_sapling"};
     public static final Block[] PLANTS = new Block[]{Blocks.OAK_SAPLING,
             Blocks.SPRUCE_SAPLING,
             Blocks.BIRCH_SAPLING,
@@ -34,6 +35,9 @@ public class ModUtil {
             Blocks.CRIMSON_FUNGUS,
             Blocks.WARPED_FUNGUS,
             Blocks.CRIMSON_ROOTS,
-            Blocks.WARPED_ROOTS};
+            Blocks.WARPED_ROOTS,
+            ModBlocks.CHERRY_BLOSSOM_SAPLING
+    };
+
     public static Block[] NEW_PLANTS = new Block[]{Blocks.CRIMSON_FUNGUS, Blocks.WARPED_FUNGUS, Blocks.CRIMSON_ROOTS, Blocks.WARPED_ROOTS};
 }
